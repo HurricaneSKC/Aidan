78f926e8ebbbf0f7d6061c4e7bf3977657cb319a
Sanjay Chopra <sanjaykchopra@gmail.com>
2023-06-13 20:28:41 +0100

    Fixed linting error

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <div className="absolute w-full border-t border-gray-200 bg-white py-5 text-center">
       <p className="text-gray-500">
-        &copy; 2021 Aidan's Portfolio. All rights reserved.
+        2021 Aidan Portfolio. All rights reserved.
       </p>
     </div>
   );

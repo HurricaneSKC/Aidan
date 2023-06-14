962f69c360a298b0747b2c5b5702f6d408d11644
Sanjay Chopra <sanjaykchopra@gmail.com>
2023-06-14 23:17:34 +0100

    slight fix on seacrh needs more

--- components/home/search-grid.tsx
+++ components/home/search-grid.tsx
@@ -11,18 +11,18 @@ export default function SearchGrid() {
   const { DemoModal, setShowDemoModal } = useDemoModal(searchQuery, setSearchQuery);
   
   return (
-    <div className="grid grid-cols-1 gap-5 md:grid-cols-3">
+    <div className="grid grid-cols-1 gap-y-5 md:gap-5 md:grid-cols-3 mx-auto">
       <DemoModal />
       <input
         type="text"
         value={searchQuery}
         onChange={(e) => setSearchQuery(e.target.value)}
         placeholder="Search..."
-        className="col-span-2 flex w-96 items-center justify-center rounded-md border border-gray-300 px-3 py-2 transition-all duration-75 hover:border-gray-800 focus:outline-none active:bg-gray-100"
+        className="col-span-2 flex w-48 md:w-80 lg:w-96 items-center justify-center rounded-md border border-gray-300 px-3 py-2 transition-all duration-75 hover:border-gray-800 focus:outline-none active:bg-gray-100"
       />
       <button
         onClick={() => setShowDemoModal(true)}
-        className="col-span-1 flex w-40 items-center justify-center rounded-md border border-gray-300 px-3 py-2 transition-all duration-75 hover:border-gray-800 focus:outline-none bg-[#005eb8] active:bg-gray-100"
+        className="col-span-1 flex w-48 items-center justify-center rounded-md border border-gray-300 px-3 py-2 transition-all duration-75 hover:border-gray-800 focus:outline-none bg-[#005eb8] active:bg-gray-100"
       >
         <p className="text-white">Search</p>
       </button>
